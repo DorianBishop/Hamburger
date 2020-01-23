6f355628fb3c01d1b82686bfa6bcd9665b895011
Dorian Bishop <dorianbishop427@gmail.com>
2020-01-23 17:15:24 -0500

    added database function and object calls

--- App/Burger.js
+++ App/Burger.js
@@ -3,3 +3,23 @@ var burger = {
     // Select all burger table entries
     selectAll: function(cb) {
       orm.selectAll('burgers', function(res) {
+        cb(res);
+    });
+  },
+
+  // This function calls the ORM single insert function.
+  insertOne: function(cols, vals, cb) {
+    orm.insertOne('burgers', cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  // objColVals is an object specifying columns as object keys with associated values
+  updateOne: function(objColVals, condition, cb) {
+    orm.updateOne('burgers', objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions for use by burgers_controller.js, which is the overall controller driver
+module.exports = burger;
